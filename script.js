c07556538001ef150e7e050cea4d9dc0690d3d26
Sujit Akulwar <sujitakulwar@gmail.com>
2022-08-04 21:35:54 +0530

    3

---
+++ script.js
@@ -0,0 +1,19 @@
+var height = 0;
+var price = 0;
+var sort = 0;
+
+function h(v){
+    height = v;
+}
+
+function p(v){
+    price = v;
+}
+
+function s(v){
+    sort = v;
+}
+
+function save(){
+    
+}

7b4946bd91cc8becb8747db3d30c03dcda1ec5b2
Sujit Akulwar <sujitakulwar@gmail.com>
2022-08-05 14:27:06 +0530

    4

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ var sort = 0;
 
 function h(v){
     height = v;
+    $(".btn").css("color", "white");
+    $(".btn").css("background-color", "rgb(108, 117, 125)");
+    if(height==1){
+        $(".1").css("color", "red");
+        $(".1").css("background-color", "skyblue");
+    }
 }
 
 function p(v){
@@ -16,5 +22,6 @@ function s(v){
 
 function save(){
     console.log(height,price,sort);
-    
+    $(".item").hide();
+    sorting(height,price);
 } 
